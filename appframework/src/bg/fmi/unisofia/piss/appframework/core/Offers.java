24a7f53113c279ecda7692c741cd4b24cb55b280
gjnedeva <gloria.nedeva@gmail.com>
2015-01-10 22:42:57 +0200

    representation/ivy.xml

--- appframework/src/bg/fmi/unisofia/piss/appframework/core/Offers.java
+++ appframework/src/bg/fmi/unisofia/piss/appframework/core/Offers.java
@@ -5,10 +5,123 @@
  */
 package bg.fmi.unisofia.piss.appframework.core;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Gloria
  */
+@Entity
+@Table(name = "Offers")
 public class Offers {
-    
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "startDate")
+    private int startDate;
+
+    @Column(name = "endDate")
+    private int endDate;
+
+    @Column(name = "destination")
+    private String destination;
+
+    public Offers(String title, String description, double price, int startDate, int endDate, String destination, int id) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.destination = destination;
+        this.id = id;
+    }
+
+    public Offers() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(int endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void addOffer(Offers offer) {
+
+    }
+
+    public void deleteOffer(Offers offer) {
+
+    }
+
+    public void editOffer(Offers offer) {
+
+    }
 }
