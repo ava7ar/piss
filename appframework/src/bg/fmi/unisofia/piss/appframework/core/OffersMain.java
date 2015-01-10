4d7c30250487baf3b02ab79744d8defaff88725b
gjnedeva <gloria.nedeva@gmail.com>
2015-01-10 22:51:12 +0200

    offersMain class

---
+++ appframework/src/bg/fmi/unisofia/piss/appframework/core/OffersMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bg.fmi.unisofia.piss.appframework.core;
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+/**
+ *
+ * @author Gloria
+ */
+public class OffersMain {
+    private static SessionsFactory factory;
+    public static void main(String[] args) {
+        
+    }
+    
+}
