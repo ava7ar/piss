3173fe184e136fc76cbb2c055209b9f9eb4b4131
apanayotov <a_panayotov@mail.bg>
2015-01-11 20:04:30 +0200

    Misc

--- representation/src/bg/fmi/unisofia/piss/representation/resource/DummyResource.java
+++ representation/src/bg/fmi/unisofia/piss/representation/resource/DummyResource.java
@@ -2,9 +2,17 @@ package bg.fmi.unisofia.piss.representation.resource;
 
 import javax.jws.WebService;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+import bg.fmi.unisofia.piss.appframework.core.DummyBean;
+import bg.fmi.unisofia.piss.appframework.core.DatabasePersister;
 
 @WebService
 @Path(value = "/test")
@@ -17,5 +25,16 @@ public class DummyResource {
             
             
 		return "Kur";
+	}
+	
+	@GET
+	@Path(value = "hibernate")
+	public String transaction() {
+		DummyBean db = new DummyBean();
+		db.setDummyName("asd");
+		db.setDummyValue("kur");
+		db.setId(123);
+		DatabasePersister.persist(db);
+		return "Successful";
 	}
 }
