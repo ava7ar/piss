e37e743cc050922d31c575ddd8ee76c37f9d6c94
mmilev <mmilev@mmilev-ThinkPad-Edge-E530c>
2015-01-12 02:39:37 +0200

    OfferRecource Get params

--- representation/src/bg/fmi/unisofia/piss/representation/resource/OffersRecource.java
+++ representation/src/bg/fmi/unisofia/piss/representation/resource/OffersRecource.java
@@ -1,22 +1,42 @@
 package bg.fmi.unisofia.piss.representation.resource;
 
-import javax.jws.WebService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import bg.fmi.unisofia.piss.appframework.core;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import bg.fmi.unisofia.piss.appframework.core.Offers;
+import bg.fmi.unisofia.piss.appframework.core.DatabasePersister;
 
-@WebService
+
 @Path(value = "/offers")
 public class OffersRecource {
 
 	@GET
 	@Path(value = "/add")
-	@Produces(MediaType.APPLICATION_JSON)
-        
-	public String add() {
-         Offers addOffer = new Offers();
+public Response offer (
+        @PathParam(value="title") String title,
+        @PathParam(value="description") String description,
+        @PathParam(value="destination") String destination,
+        @PathParam(value="price") double price,
+        @PathParam(value="startDate") String startDate,
+        @PathParam(value="endDate") String endDate,
+        @PathParam(value="imageUrl") String imageUrl)         
+	 {
+             try{
+            Offers offer = new Offers();
+            offer.setTitle(title);
+            offer.setDescription(description);
+            offer.setPrice(price);
+            offer.setStartDate(startDate);
+            offer.setEndDate(endDate);
+            offer.setStartDate(startDate);
+            offer.setDestination(destination);
+            offer.setImageUrl(imageUrl);
+            DatabasePersister.persist(offer);
+             }catch(RuntimeException e){
+                 throw e;
+             }
 	}
+             
 }
 
